c5418fd2ec93b27d16bc3ce2afb43a8f6d493196
Abdulrhman <obeidgad@gmail.com>
2021-05-16 23:19:21 +0200

    UPD

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "pos_retail_cubic",
+    'summary': """POS cubic custom """,
+    'description': """POS cubic custom """,
+    'author': "",
+    'category': 'Uncategorized',
+    'version': '13.0.0.1',
+    'depends': ['base', 'point_of_sale', 'pos_retail'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/stock_warehouse.xml',
+    ],
+    'qweb': [
+        'static/src/xml/Report.xml',
+    ],
+
+}
---
+++ models/warehouse.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+class StockWarehouse(models.Model):
+    _inherit = 'stock.warehouse'
+
+    pos_branch_id = fields.Many2one(
+        'pos.branch',
+        string='Branch',
+    )
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('pos_branch_id'):
+            vals.update({'pos_branch_id': self.env['pos.branch'].sudo().get_default_branch()})
+        warehouse = super(StockWarehouse, self).create(vals)
+        return warehouse
+
+    warehouse_product_ids = fields.One2many('stock.quant', 'location_id', string='Available Products',
+                                            compute='compute_warehouse_products')
+
+    def compute_warehouse_products(self):
+        for products in self:
+            warehouse_all_products = self.env['stock.quant'].search([('location_id', 'child_of', self.code)])
+            if warehouse_all_products:
+                for i in warehouse_all_products:
+                    products.warehouse_product_ids = warehouse_all_products
+            else:
+                products.warehouse_product_ids = False
+
+
+class StockPickingType(models.Model):
+    _inherit = 'stock.picking.type'
+
+    pos_branch_id = fields.Many2one(
+        'pos.branch',
+        related='warehouse_id.pos_branch_id',
+        store=True,
+        readonly=1
+    )
