87d12029bc4ecfae045f3b062125bc0952eb736b
powerfulman86 <dba.ahmedmaher@gmail.com>
2021-07-15 08:36:22 +0200

    Update report

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
+
 from . import pos_session
 from . import warehouse
 from . import pos_config
 from . import PosOrder
 from . import pos_order_line
-from . import pos_sale_analytic
-from . import warehouse_pos_branch_report
-
---
+++ report/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import pos_sale_analytic
+from . import warehouse_pos_branch_report
+from . import warehouse_move_branch
